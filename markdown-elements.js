26e1d9025599b6ddea965db1ca6a3bb0c33e1490
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-05-20 00:09:45 -0400

    Update markdown-elements.js

--- markdown-elements.js
+++ markdown-elements.js
@@ -3,10 +3,13 @@
 //# sourceMappingURL=showdown.min.js.map
 
 
-	var converter = new showdown.Converter();
-			converter.setOption('tables', 'on');
+var converter = new showdown.Converter()
+
+converter.setOption('tables', 'on')
 			
-			converter.setOption('emoji', 'on');
+			converter.setOption('emoji', 'on')
+
+converter.setOption('strikethrough', 'on');
 			elements = document.getElementsByTagName("md");
 			for(var i=0; i<elements.length; i++)
 				elements[i].innerHTML = converter.makeHtml(elements[i].textContent)
