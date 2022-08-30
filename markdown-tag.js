d00be732333fe0a9fb8ba462590ed78c7917c11d
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-30 05:18:45 -0400

    Added Attribute Option :pencil:

--- markdown-tag.js
+++ markdown-tag.js
@@ -1,5 +1,5 @@
 /**!
- * @license Markdown-Tag - Add Markdown to any HTML using a <md> tag
+ * @license Markdown-Tag - Add Markdown to any HTML using a <md> tag or md attribute
  * LICENSED UNDER GPL-3.0 LICENSE
  * MARKDOWN FLAVOUR: STANDARD FLAVORED MARKDOWN. 
  * MORE INFO / FLAVOR OPTIONS CAN BE FOUND AT https://github.com/MarketingPipeline/Markdown-Tag/
@@ -27,8 +27,62 @@ function addCss(fileName) {
 
 }
 
-/* Convert Markdown Tags */ 
+/* Convert Markdown Attributes */ 
 
+  
+ 
+if (document.querySelectorAll('[md]').length > 0) {
+  
+  var converter = new showdown.Converter() 
+converter.setOption('tables', 'on')
+			
+converter.setOption('emoji', 'on')
+
+converter.setOption('strikethrough', 'on');
+  
+MD_TAG = document.querySelectorAll('[md]');
+for(var i=0; i<MD_TAG.length; i++)
+	  /// Temp fix for Gitub Block Quotes
+MD_TAG[i].innerHTML = converter.makeHtml(MD_TAG[i].innerHTML.replace(/&gt;/g, '>'))
+}
+
+  
+
+if (document.querySelectorAll('[github-md]').length > 0) {
+  
+addCss('https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Elements/stylesheets/github_md.css');
+ 
+
+  var converter = new showdown.Converter() 
+  
+converter.setOption('tables', 'on')
+			
+converter.setOption('emoji', 'on')
+
+converter.setOption('strikethrough', 'on')
+
+converter.setOption('tasklists', 'true')
+
+
+converter.setOption('ghMentions', 'true')
+
+
+converter.setOption('simplifiedAutoLink', 'true');
+
+  
+  
+GitHub_MD_TAG = document.querySelectorAll('[github-md]');
+for(var i=0; i<GitHub_MD_TAG.length; i++)
+  /// Temp fix for Gitub Block Quotes
+   GitHub_MD_TAG = GitHub_MD_TAG[i]
+  GitHub_MD_TAG.classList.add("github-md");
+GitHub_MD_TAG.innerHTML = converter.makeHtml(GitHub_MD_TAG[i].innerHTML.replace(/&gt;/g, '>'))
+
+}  
+  
+
+ /* Convert Markdown Tags */ 
+  
 if (document.getElementsByTagName("md").length > 0) {
   
   var converter = new showdown.Converter() 
