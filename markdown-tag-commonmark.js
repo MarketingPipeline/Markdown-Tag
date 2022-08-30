970c91b698bf98eec447541ab5ff875d8ae40205
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-30 00:15:59 -0400

    Updated Markdown Tag - CommonMark :pencil:

--- markdown-tag-commonmark.js
+++ markdown-tag-commonmark.js
@@ -1,3 +1,9 @@
+/**!
+ * @license Markdown-Tag - Add Markdown to any HTML using a <md> tag
+ * LICENSED UNDER GPL-3.0 LICENSE
+ * MARKDOWN FLAVOUR: COMMONMARK FLAVORED MARKDOWN. 
+ * MORE INFO / FLAVOR OPTIONS CAN BE FOUND AT https://github.com/MarketingPipeline/Markdown-Tag/
+ */
 var Debug = false;
 function renderMarkdown(){
   
