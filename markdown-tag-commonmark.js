8ebfc33ad740e9d91026d987785a5d3e95c27c63
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-30 00:32:26 -0400

    Updated Markdown Tag - CommonMark :pencil:

--- markdown-tag-commonmark.js
+++ markdown-tag-commonmark.js
@@ -7,7 +7,7 @@
 var Debug = false;
 function renderMarkdown(){
   
-/* Add Github CSS + Syntax Highlight CSS  */  
+/* Add Github CSS  */  
 
 
 var CSSAdded = false;
@@ -28,25 +28,6 @@ function addCss(fileName) {
 
 }
 
-
-
-var SyntaxCSSAdded = false;  
-function addSyntaxHighlightCss(fileName) {
-
-  if (SyntaxCSSAdded == false){
-      var head = document.head;
-  var link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = fileName;
-
-  head.appendChild(link);
-  SyntaxCSSAdded = true
-   
-  } 
-
-}
   
 if (document.getElementsByTagName("md").length > 0) {
 
@@ -63,8 +44,7 @@ if (document.getElementsByTagName("github-md").length > 0) {
 
 addCss('https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Elements/stylesheets/github_md.css');  
  
-  addSyntaxHighlightCss('https://cdn.jsdelivr.net/gh/PrismJS/prism-themes/themes/prism-ghcolors.css')
-  addSyntaxHighlighter()
+
 var GitHub_MD_TAG = document.getElementsByTagName("github-md");
 for(var i=0; i<GitHub_MD_TAG.length; i++)
 GitHub_MD_TAG[i].innerHTML = marked.parse(GitHub_MD_TAG[i].innerHTML)
@@ -74,35 +54,6 @@ GitHub_MD_TAG[i].innerHTML = marked.parse(GitHub_MD_TAG[i].innerHTML)
 
 
 
-function addSyntaxHighlighter(){
-  
-  /// Add Prism.JS to document
-var script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/npm/prismjs@1.28.0/prism.min.js";
-
-document.head.appendChild(script); 
-  
-  
-  
-  
-  // On Error Loading Markdown Parser
-script.onerror = function () {
- 
-  console.error("Markdown Tag: Error while performing function addSyntaxHighlighter - There was an error loading the Syntax Highlighter");
-  
-}
-
-  
-  /// Markdown Parser Load Successful 
-script.onload = function () {
- 
-  if (Debug == true){
-    console.log("Syntax Highlighter Was Loaded")
-  }
-
-  
-};
-}
 
 
 function loadMarkdownParser(){
